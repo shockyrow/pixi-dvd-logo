8004b561c45f76331d72bf0d4741ddc8c9d2376f
Ibrohim Shokirov <ishokirov@hotmail.com>
2026-04-27 10:59:46 +0700

    Created DVD logo simulation

---
+++ src/Logo.ts
@@ -0,0 +1,103 @@
+import type { Bounds, Updatable } from "./interfaces";
+
+interface LogoView {
+  x: number;
+  y: number;
+}
+
+export class Logo implements Updatable {
+  private x: number;
+  private y: number;
+  private w: number;
+  private h: number;
+  private dx: number = 1;
+  private dy: number = 1;
+  private stepSize: number = 2;
+  private bounds: Bounds;
+  private view: LogoView;
+  private dragging: boolean = false;
+
+  constructor(
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    bounds: Bounds,
+    view: LogoView,
+  ) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.bounds = bounds;
+
+    if (Math.random() > 0.5) {
+      this.dx *= -1;
+    }
+
+    if (Math.random() > 0.5) {
+      this.dy *= -1;
+    }
+
+    this.view = view;
+
+    this.view.x = this.x;
+    this.view.y = this.y;
+  }
+
+  public moveBy(dx: number, dy: number): void {
+    this.x += dx;
+    this.y += dy;
+
+    this.view.x = this.x;
+    this.view.y = this.y;
+  }
+
+  public isDragging(): boolean {
+    return this.dragging;
+  }
+
+  public startDragging() {
+    this.dragging = true;
+  }
+
+  public stopDragging()
+  {
+    if (this.x < 0) {
+      this.x = 0;
+    } else if (this.x + this.w >= this.bounds.width) {
+      this.x = this.bounds.width - this.w - 1;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+    } else if (this.y + this.h >= this.bounds.height) {
+      this.y = this.bounds.height - this.h - 1;
+    }
+    this.dragging = false;
+  }
+
+  public setStepSize(stepSize: number) {
+    this.stepSize = stepSize;
+  }
+
+  public update(): void {
+    if (!this.dragging) {
+      this.x += this.dx * this.stepSize;
+    this.y += this.dy * this.stepSize;
+
+    if (this.x + this.w >= this.bounds.width || this.x < 0) {
+      this.dx *= -1;
+      this.x += this.dx * this.stepSize;
+    }
+
+    if (this.y + this.h >= this.bounds.height || this.y < 0) {
+      this.dy *= -1;
+      this.y += this.dy * this.stepSize;
+    }
+
+    this.view.x = this.x;
+    this.view.y = this.y;
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,100 @@
 import "./style.css";
-import { Application, Graphics, Particle, ParticleContainer } from "pixi.js";
-import Stats from "stats.js";
-import { Walker } from "./Walker";
+import dvdLogo from "./assets/dvd-logo.svg";
+import { Application, Assets, Sprite } from "pixi.js";
+import { Logo } from "./Logo";
 import type { Updatable } from "./interfaces";
-import WalkerWorker from "./walker.worker?worker";
 import { Pane } from "tweakpane";
 
 const updatables: Updatable[] = [];
-const stats = new Stats();
 const app = new Application();
 
-let container: ParticleContainer;
-let worker: Worker | null = null;
+const visual = new Sprite(await Assets.load(dvdLogo));
+visual.scale.set(0.5);
+visual.tint = "gold";
+visual.eventMode = "dynamic";
 
-const params = {
-  walkerCount: 10000,
-  reset: () => resetSimulation(),
+let logo: Logo | null = null;
+let lastX = 0;
+let lastY = 0;
+
+visual.on("pointerdown", () => {
+  if(logo) logo.startDragging()
+});
+
+const pointerUpHandler = () => {
+  if(logo) logo.stopDragging()
 };
 
-function resetSimulation() {
-  // 1. Cleanup existing state
-  if (worker) worker.terminate();
-  container.removeParticles();
-  updatables.length = 0;
+visual.on("pointerup", pointerUpHandler);
+visual.on("pointerupoutside", pointerUpHandler);
 
-  const walkerVisual = new Graphics().circle(0, 0, 5).fill(0xffffff);
-  const walkerTexture = app.renderer.generateTexture(walkerVisual);
-  walkerVisual.destroy();
+visual.on("globalpointermove", (e) => {
+  if (logo && logo.isDragging()) logo.moveBy(e.clientX - lastX, e.clientY - lastY);
 
-  // 3. Start Worker
-  worker = new WalkerWorker();
-  worker.postMessage({
-    count: params.walkerCount,
-    bounds: app.screen,
-  });
+  lastX = e.clientX;
+  lastY = e.clientY;
+});
 
-  worker.onmessage = (e: MessageEvent) => {
-    const { type, data } = e.data;
-
-    if (type === "BATCH") {
-      data.forEach((datum: { x: number; y: number }) => {
-        const visual = new Particle(walkerTexture);
-        container.addParticle(visual);
-
-        const walker = new Walker(datum.x, datum.y, app.screen, visual);
-        updatables.push(walker);
-      });
-    }
-
-    if (type === "COMPLETE" && worker) {
-      worker.terminate();
-    }
-  };
-}
+const params = {
+  background: '#161616',
+  tint: '#ff0',
+  stepSize: 2,
+};
 
 function setupUI() {
   const controlsPane = new Pane({ title: "Simulation controls" });
 
-  controlsPane.addBinding(params, "walkerCount", {
-    min: 100,
-    max: 100000,
-    step: 100,
-    label: "Count",
-  });
+  controlsPane
+    .addBinding(params, "background", {
+      label: "Background",
+    })
+    .on("change", (e) => {
+      app.renderer.background.color = e.value;
+    });
 
-  controlsPane.addButton({ title: "Restart" }).on("click", () => {
-    resetSimulation();
-  });
+  controlsPane
+    .addBinding(params, "tint", {
+      label: "Tint",
+    })
+    .on("change", (e) => {
+      visual.tint = e.value;
+    });
+
+  controlsPane
+    .addBinding(params, "stepSize", {
+      label: "Step Size",
+      min: .1,
+      max: 10,
+    })
+    .on("change", (e) => {
+      logo.setStepSize(e.value)
+    });
 }
 
 async function init() {
   await app.init({
-    background: "#1099bb",
+    background: "#161616",
     resizeTo: window,
   });
 
   document.body.appendChild(app.canvas);
 
-  stats.showPanel(0);
-  document.body.appendChild(stats.dom);
-
-  container = new ParticleContainer({
-    dynamicProperties: { position: true },
-  });
-
-  app.stage.addChild(container);
-
   setupUI();
-  resetSimulation();
+
+  app.stage.addChild(visual);
+
+  logo = new Logo(
+    Math.random() * (app.screen.width - visual.width),
+    Math.random() * (app.screen.height - visual.height),
+    visual.width,
+    visual.height,
+    app.screen,
+    visual,
+  );
+  updatables.push(logo);
 
   app.ticker.add(() => {
-    stats.begin();
-
     updatables.forEach((updatable) => updatable.update());
-
-    stats.end();
   });
 }
 
